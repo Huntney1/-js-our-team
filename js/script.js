ef18f0febcb06120ef814c922bc24835713213b5
Huntney1 <devilwhiteone@hotmail.it>
2022-12-13 17:40:17 +0100

    mileston 1 / 2

--- js/script.js
+++ js/script.js
@@ -1,51 +1,57 @@
-let team = [
-    {
-        nome: "Wayne Barnett",
-        cognome: "Barnett",
-        role: "Founder & CEO",
-        image:"wayne-barnett-founder-ceo.jpg",
-    },
-    {
-        nome: "Angela",
-        cognome: "Caroll",
-        role: "Chief Editor",
-        image:"angela-caroll-chief-editor.jpg",
+//variabili
 
-    },
-    {
-        nome: "Walter",
-        cognome: "Gordon",
-        role: "Office Manager",
-        image: "walter-gordon-office-manager.jpg",
-    },
+let container = document.getElementById('container');
+let persona, div, teams;
 
-    {
-        nome:"Angela" ,
-        cognome:"Lopez" ,
-        role: "Social Media Manager",
-        image:"angela-lopez-social-media-manager.jpg",
 
-    },
+function creaDiv(){
+    div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `<p>${teams.nome}<br>${teams.role}</p> <img src="./img/${teams.image}">`;
+    return div;
+}
 
-    {
-        nome:"Scott " ,
-        cognome:"Estrada" ,
-        role: "Developer",
-        image:"scott-estrada-developer.jpg",
+//oggetto
 
-    },
-
-    {
-        nome:"Barbara" ,
-        cognome:"Ramos" ,
-        role: "Graphic Designer",
-        image:"barbara-ramos-graphic-designer.jpg",
-
-    }
+let team = [{
+    nome: "Wayne Barnett",
+    role: "Founder & CEO",
+    image: "wayne-barnett-founder-ceo.jpg"
+},
+{
+    nome: "Angela Caroll",
+    role: "Chief Editor",
+    image: "angela-caroll-chief-editor.jpg"
+},
+{
+    nome: "Walter Gordon",
+    role: "Office Manager",
+    image: "walter-gordon-office-manager.jpg"
+},
+{
+    nome: "Angela Lopez",
+    role: "Social Media Manager",
+    image: "angela-lopez-social-media-manager.jpg"
+},
+{
+    nome: "Scott Estrada",
+    role: "Developer",
+    image: "scott-estrada-developer.jpg"
+},
+{
+    nome: "Barbara Ramos",
+    role: "Graphic Designer",
+    image: "barbara-ramos-graphic-designer.jpg"
+}
 ]
 
-
 for(let i = 0; i<team.length; i++){
-    let teams = team[i];
-    console.log(teams)
+
+    //stampa console
+    teams = team[i];
+    console.log(teams);
+    //stampa dom
+    creaDiv(teams);
+    container.appendChild(div);
+
 }
